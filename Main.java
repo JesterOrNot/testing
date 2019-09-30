6e43f33faec57eba822b3e47fd2d4d451298539c
Sean Hellum <seanhellum45@gmail.com>
2019-09-30 17:03:51 +0000

    new

--- Main.java
+++ Main.java
@@ -29,7 +29,7 @@ class Main {
         BinaryTree tree = new BinaryTree();
         double targetVal = 0.5;
         tree.root = new Node(5);
-        tree.root.left = new Node(3);
+        turee.root.left = new Node(3);
         tree.root.right = new Node(7);
         tree.root.left.left = new Node(1);
         tree.root.left.left.left = new Node(0.5);
