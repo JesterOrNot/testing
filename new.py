6751a6ae8a895781cfa5cc385649b908fa957b72
Sean Hellum <seanhellum45@gmail.com>
2019-10-01 23:40:25 +0000

    new

--- new.py
+++ new.py
@@ -1,2 +1,10 @@
-for i in range(10):
-    print(i)
+import json
+x = {"hello":33,"new":2019}
+with open("new.json",'w+') as write_file:
+    json.dump(x,write_file,indent=4)
+with open("new.json","r") as read_file:
+    data = json.load(read_file)
+z = data.get("hello")
+b = data.get('new')
+print(z)
+print(b)
