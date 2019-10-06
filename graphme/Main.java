a14256baba9984f2f9c07326cd21e745bcba33ce
Sean Hellum <seanhellum45@gmail.com>
2019-10-06 23:37:46 +0000

    z

--- graphme/Main.java
+++ graphme/Main.java
@@ -1,13 +1,28 @@
 import java.io.IOException;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 public class Main {
     public static void main(String[] args) {
-        ProcessBuilder pb = new ProcessBuilder("python3","graph.py");
-        try {
-			Process p = pb.start();
-		} catch (IOException e) {
-			// Auto-generated catch block
-			e.printStackTrace();
-        }
+        JFrame frame = new JFrame("Grapher");
+        JButton button = new JButton("Submit");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                ProcessBuilder pb = new ProcessBuilder("python3","graph.py");
+                try {
+                    Process p = pb.start();
+                } catch (IOException e) {
+                    // Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+        // ProcessBuilder pb = new ProcessBuilder("python3","graph.py");
+        // try {
+		// 	Process p = pb.start();
+		// } catch (IOException e) {
+		// 	// Auto-generated catch block
+		// 	e.printStackTrace();
+        // }
     }
 }
