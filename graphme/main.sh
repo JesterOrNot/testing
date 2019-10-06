e862d53445429a65dbc3b2fcc63f97846015a432
Sean Hellum <seanhellum45@gmail.com>
2019-10-06 22:27:26 +0000

    2,3,4

--- graphme/main.sh
+++ graphme/main.sh
@@ -1,6 +1,6 @@
 python3 clear.py
-read -p "What are the x coords" z
-read -p "What are the y coords" v
+read -p "What are the x coords?: " z
+read -p "What are the y coords?: " v
 echo $z &>> stdin.txt
 echo $v &>> stdin.txt
 java Main
